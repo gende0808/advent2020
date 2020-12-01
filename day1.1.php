1640223f04f3cc359d4e352ae2f705e2d38f6a56
Gregory <gende@novacollege.nl>
2020-12-01 13:00:11 +0100

    slight upgrade to code and 1.2 done.

--- day1.1.php
+++ day1.1.php
@@ -1,7 +1,7 @@
 <?php
 $array = explode("\n",file_get_contents("day1.txt"));
 for($i = 0; $i < count($array); $i++){
-    for($j = $i; $j < count($array); $j++){
+    for($j = $i + 1; $j < count($array); $j++){
         if((int)$array[$i] + (int)$array[$j] == 2020){
             echo (int)$array[$i] * (int)$array[$j];
         }
--- day1.2.php
+++ day1.2.php
@@ -1,1 +1,11 @@
 <?php
+$array = explode("\n",file_get_contents("day1.txt"));
+for($i = 0; $i < count($array); $i++){
+    for($j = $i + 1; $j < count($array); $j++){
+        for($k = $j + 1; $k < count($array); $k++) {
+            if((int)$array[$k] + (int)$array[$j] + (int)$array[$i] == 2020){
+                echo (int)$array[$k] * (int)$array[$j] * (int)$array[$i];
+            }
+        }
+    }
+}
