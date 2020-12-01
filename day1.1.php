7658d8218c71b2f0db01cc3062148ad4a8d7e92d
Gregory <gende@novacollege.nl>
2020-12-01 12:55:46 +0100

    minor update

--- day1.1.php
+++ day1.1.php
@@ -1,8 +1,5 @@
 <?php
-include "functions.php";
-
 $array = explode("\n",file_get_contents("day1.txt"));
-
 for($i = 0; $i < count($array); $i++){
     for($j = $i; $j < count($array); $j++){
         if((int)$array[$i] + (int)$array[$j] == 2020){
