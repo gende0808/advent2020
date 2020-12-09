83f7602b2cd5139984cf508bc794c7111268b593
gregory van den Ende <gende@novacollege.nl>
2020-12-09 09:46:45 +0100

    day 8 part two done

--- day8.2.php
+++ day8.2.php
@@ -1,27 +1,24 @@
 <?php
-
-include "functions.php";
 $array = explode("\n", file_get_contents("day8.txt"));
 $arrayOfIndexes = array();
 $accumulator = 0;
 
 letsGetWild($array);
-function letsGetWild($array, $changeAfter = 0, $accumulator = 1, $arrayOfIndexes = array(), $changed = false)
+function letsGetWild($array, $changeAfter = 3, $accumulator = 0, $arrayOfIndexes = array(), $changed = false)
 {
     if ($changeAfter > count($array) - 1) {
-        tryTheOtherRide();
     }
-    if (strpos($array[$changeAfter], "nop") !== false) {
-        $array[$changeAfter] = str_replace("nop", "jmp", $array[$changeAfter]);
+    if (strpos($array[$changeAfter], "jmp") !== false) {
+        $array[$changeAfter] = str_replace("jmp", "nop", $array[$changeAfter]);
         $changed = true;
     }
     for ($i = 0; $i < count($array); $i++) {
-       $instruction = explode(" ", $array[$i])[0];
+        $instruction = explode(" ", $array[$i])[0];
         $amount = explode(" ", $array[$i])[1];
         //if we've been here before, time to go byebye
         if (isset($arrayOfIndexes[$i])) {
             if ($changed == true) {
-                $array[$changeAfter] = str_replace("jmp", "nop", $array[$changeAfter]);
+                $array[$changeAfter] = str_replace("nop", "jmp", $array[$changeAfter]);
             }
             $changeAfter++;
             letsGetWild($array, $changeAfter);
@@ -33,23 +30,14 @@ function letsGetWild($array, $changeAfter = 0, $accumulator = 1, $arrayOfIndexes
             $arrayOfIndexes[$i] = $instruction . " " . $amount;
         }
         if ($instruction == "acc") {
-            $accumulator += (int)trim($amount) ;
+            $accumulator += (int)trim($amount);
         }
         if ($instruction == "jmp") {
-            $i += (int)$amount;
-
+            $i += (int)trim($amount) - 1;
+//            echo $i . $instruction . $amount."<br>";
         }
-        if(isset($arrayOfIndexes[count($array)-1])){
+        if (isset($arrayOfIndexes[count($array) - 1])) {
             echo "found it: $accumulator";
-            print_array($arrayOfIndexes);
-            exit;
         }
-
     }
-}
-
-function tryTheOtherRide()
-{
-    echo "this is other ride speaking";
-    exit;
 }
