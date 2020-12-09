57a8b45346cbe67f1dada933d46081c7bd0d1a8a
gregory van den Ende <gende@novacollege.nl>
2020-12-09 17:46:03 +0100

    TO progress or not to progress. azeth stop reading these commit messages.

--- day7.2.php
+++ day7.2.php
@@ -17,16 +17,16 @@ function checkArray($array, $searchFor, $totalBags = 0, $multiplier = 0)
     foreach ($searchFor as $item => $amount) {
         foreach ($array as $bagWithBags) {
             if (strpos(trim($bagWithBags[0]), trim($item)) !== false) {
-                for ($i = 0; $i < count($bagWithBags); $i++) {
+                for ($i = 1; $i < count($bagWithBags); $i++) {
                     $bagBits = explode(" ", $bagWithBags[$i], 2);
                     if (is_numeric($bagBits[0])) {
-                            $newSearchFor[trim($bagBits[1])] = $bagBits[0];
+                        $newSearchFor[trim($bagBits[1])] = $bagBits[0];
+                        $totalBags = $totalBags * $bagBits[0] + $totalBags;
                     }
                 }
-            }
+             }
         }
     }
-
-    print_array($newSearchFor);
-    checkArray($array, $newSearchFor, $totalBags, $multiplier);
+    print_array($searchFor);
+    checkArray($array, $newSearchFor,$totalBags, $multiplier);
 }
